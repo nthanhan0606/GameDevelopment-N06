aed37b1810f16c88f8a85d4d6ea8ff3ea5dfba28
Nguyen Thanh An <antui102098@gmail.com>
2026-01-27 14:08:05 +0700

    feat: demo part 3 implementation, fixes #2

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -4,25 +4,30 @@ public class PlayerShooting : MonoBehaviour
 {
     public GameObject bulletPrefabs;
     public float shootingInterval;
-    public Vector3 bulletSpawnOffset = new(0, 0, 1f);
+    public Vector3 bulletOffset;
 
     private float lastBulletTime;
-    
+
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            if (Time.time - lastBulletTime > shootingInterval)
-            {
-                ShootBullet();
-                lastBulletTime = Time.time;
-            }
+            UpdateFiring();
+        }
+    }
+
+    private void UpdateFiring()
+    {
+        if (Time.time - lastBulletTime > shootingInterval)
+        {
+            ShootBullet();
+            lastBulletTime = Time.time;
         }
     }
 
     private void ShootBullet()
     {
-        Vector3 spawnPosition = transform.position + transform.TransformDirection(bulletSpawnOffset);
-        Instantiate(bulletPrefabs, spawnPosition, transform.rotation);
+        var bullet = Instantiate(bulletPrefabs, transform.position + bulletOffset, transform.rotation);
     }
 }
