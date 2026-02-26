f55b1b727d3c6c51bc59cb301036c2ff1baa2c28
Nguyen Thanh An <antui102098@gmail.com>
2026-02-26 14:55:28 +0700

    feat: demo part 5 implementation, fixes #4

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -5,6 +5,11 @@ public class EnemyAttack : MonoBehaviour
     public EnemyHealth health;
     public int damage;
 
+    private void Awake()
+    {
+        health = GetComponent<EnemyHealth>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<PlayerHealth>(out var playerHealth))
